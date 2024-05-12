84824f7fca675eae0ec3ad3affcdf84367a4523e
npenin <github@dragon-angel.fr>
2024-05-12 18:00:06 +0200

    feat: add private recipes concept

---
+++ wwwroot/_data/allrecettes.js
@@ -0,0 +1,12 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+module.exports = async function ()
+{
+    let files = await fs.readdir(path.resolve('./recettes'));
+    files = files.filter(f => f.endsWith('.json'));
+    const result = await Promise.all(files.map(f_1 => fs.readFile(path.join('./recettes', f_1), { encoding: 'utf-8' }).then(r => ({ ...JSON.parse(r), filepath: path.basename(f_1, '.json') }))));
+
+    // console.log(result);
+    return result;
+};
--- wwwroot/_data/moules.js
+++ wwwroot/_data/moules.js
@@ -1,13 +1,8 @@
-const fs = require('fs/promises')
-const path = require('path')
 const { distinctStrings } = require('@akala/core')
+const recettes = require('./recettes')
 
 module.exports = async function ()
 {
-    let files = await fs.readdir(path.resolve('./recettes'));
-    files = files.filter(f => f.endsWith('.json'));
-    const result = await Promise.all(files.map(f_1 => fs.readFile(path.join('./recettes', f_1), { encoding: 'utf-8' }).then(r => JSON.parse(r))));
 
-    // console.log(result);
-    return distinctStrings(result.map(r => r.mold), m => m.name);
+    return distinctStrings((await recettes()).map(r => r.mold), m => m.name);
 };
