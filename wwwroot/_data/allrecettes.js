096f5abcc8640f31dd69e46e43b87d9bb861edde
npenin <github@dragon-angel.fr>
2024-11-24 11:36:32 +0100

    feat: upgrade the admin section to use swal feat: add multipart recipes

--- wwwroot/_data/allrecettes.js
+++ wwwroot/_data/allrecettes.js
@@ -1,5 +1,6 @@
 const fs = require('fs/promises')
 const path = require('path')
+const marked = require('marked');
 
 module.exports = async function ()
 {
@@ -8,5 +9,60 @@ module.exports = async function ()
     const result = await Promise.all(files.map(f_1 => fs.readFile(path.join('./recettes', f_1), { encoding: 'utf-8' }).then(r => ({ ...JSON.parse(r), filepath: path.basename(f_1, '.json') }))));
 
     // console.log(result);
-    return result;
+    return result.map(r =>
+    {
+        const result = {}
+        if (Array.isArray(r.toppings))
+        {
+            if (Object.entries(r.toppings[0]).filter(e => !e[1]).length == 2)
+            {
+                result.categorizedToppings = {};
+                let toppingCategory = '';
+                for (const topping of r.toppings)
+                {
+                    if (topping.unit == topping.quantity)
+                    {
+                        if (toppingCategory && result.categorizedToppings[toppingCategory])
+                            result.categorizedToppings[toppingCategory][result.categorizedToppings[toppingCategory].length - 1].name = result.categorizedToppings[toppingCategory][result.categorizedToppings[toppingCategory].length - 1].name.replace(/(  ?)+$/, '')
+                        toppingCategory = topping.name;
+                        continue;
+                    }
+                    else if (topping.name == topping.quantity)
+                    {
+                        if (toppingCategory && result.categorizedToppings[toppingCategory])
+                            result.categorizedToppings[toppingCategory][result.categorizedToppings[toppingCategory].length - 1].name = result.categorizedToppings[toppingCategory][result.categorizedToppings[toppingCategory].length - 1].name.replace(/(  ?)+/, '')
+                        toppingCategory = topping.unit;
+                        continue;
+                    }
+                    else if (topping.name == topping.unit)
+                    {
+                        if (toppingCategory && result.categorizedToppings[toppingCategory])
+                            result.categorizedToppings[toppingCategory][result.categorizedToppings[toppingCategory].length - 1].name = result.categorizedToppings[toppingCategory][result.categorizedToppings[toppingCategory].length - 1].name.replace(/(  ?)+$/, '')
+                        toppingCategory = topping.quantity;
+                        continue;
+                    }
+
+                    if (!result.categorizedToppings[toppingCategory])
+                        result.categorizedToppings[toppingCategory] = [];
+
+                    result.categorizedToppings[toppingCategory].push(topping);
+                }
+            }
+            else
+                result.categorizedToppings = { '': r.toppings };
+        }
+        else
+            result.categorizedToppings = r.toppings;
+
+        if (Array.isArray(r.steps))
+            r.formattedSteps = r.steps.reduce((previous, current, i) =>
+            {
+                if (i == 0 && /\n([^\n]{5,256})\n*$/m.test(current))
+                    return current.replace(/^([^\n]+)\n+([^\n]{5,256})\n*$/m, '### $2\n\n1. $1') + '\n'
+                return previous + (i + 1) + '. ' + current.replace(/\n([^-\n][^\n]{5,256})\n*$/, (m, g) => '\n### ' + g).replace(/\n- /g, '\n    - ') + '\n'
+            }, '');
+
+        result.formattedSteps = marked.parse(r.formattedSteps);
+        return { ...r, ...result };
+    });
 };
