710418bf567ca490c3c73d818db286faa6ccd971
Nicolas Penin <github@dragon-angel.fr>
2026-09-20 19:35:22 +0200

    feat: add ebook id to molds

--- wwwroot/_data/allmoules.js
+++ wwwroot/_data/allmoules.js
@@ -3,8 +3,17 @@ import recettes from './recettes.js'
 
 export default async function allmoules()
 {
-    return distinctStrings((await recettes()).map(r => r.mold), m => m.name).map(m => ({
-        ...m,
-        displayName: m.name.replace(/([A-Z])([A-Z]+)/g, (_, letter, letters) => letter + letters.toLowerCase()),
-    }))
+    return distinctStrings((await recettes()).map(r => r.mold), m => m.name).map(m =>
+    {
+        const ebookId = /\/(\d+)-/.exec(m.url);
+
+        if (!ebookId)
+            console.log('no id found in ' + m.url);
+
+        return {
+            ...m,
+            ebook: ebookId?.[1],
+            displayName: m.name.replace(/([A-Z])([A-Z]+)/g, (_, letter, letters) => letter + letters.toLowerCase()),
+        }
+    })
 };
